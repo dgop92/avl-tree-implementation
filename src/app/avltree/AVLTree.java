8a992dc5fbcb3edf4d0425e90720b1dbac01be1a
dgop92 <inevaup@gmail.com>
2021-08-20 19:08:35 -0500

    feat: add java doc of avl tree

--- src/app/avltree/AVLTree.java
+++ src/app/avltree/AVLTree.java
@@ -3,23 +3,33 @@ package app.avltree;
 import java.util.function.Consumer;
 
 public class AVLTree {
-    
+
     private Node root;
 
     public AVLTree() {
         this.root = null;
     }
 
-    public void insert(int data){
+    /**
+     * Insert a new data into the tree
+     * 
+     * @param data an int value
+     */
+    public void insert(int data) {
         this.root = addNode(this.root, data);
     }
 
-    public void inOrderTraversal(Consumer<Node> action){
+    /**
+     * Traverse the tree using inOrder traversal
+     * 
+     * @param action A callback function with the node being proecced
+     */
+    public void inOrderTraversal(Consumer<Node> action) {
         inOrder(this.root, action);
     }
 
     private void inOrder(Node root, Consumer<Node> action) {
-        if (root != null){
+        if (root != null) {
             inOrder(root.left, action);
             action.accept(root);
             inOrder(root.right, action);
@@ -27,43 +37,51 @@ public class AVLTree {
     }
 
     private Node addNode(Node root, int data) {
+
+        // Base case for these recursive function. 
+        // if a leaf node is reached, append the node
         if (root == null) {
             return new Node(data);
         }
 
+        // Conditionals to satisfy the BST tree invariant
         if (data < root.data) {
             root.left = addNode(root.left, data);
-        }else {
-            root.right = addNode(root.right, data); 
+        } else {
+            root.right = addNode(root.right, data);
         }
 
+        // Note: the following code is executed after inserting the node
+        // update the height of the node
         root.height = AVLTreeUtilities.getNewHeight(root);
+
+        // According to the balance of the node apply the respective rotations
 
         int balance = AVLTreeUtilities.getBalance(root);
 
         // Left Left Case
-        if (balance > 1 && data < root.left.data){
+        if (balance > 1 && data < root.left.data) {
             return rightRotate(root);
         }
 
-        // Left Right 
-        if (balance > 1 && data > root.left.data){
+        // Left Right
+        if (balance > 1 && data > root.left.data) {
             root.left = leftRotate(root.left);
             return rightRotate(root);
-        } 
+        }
 
-        // Right Right 
-        if (balance < -1 && data > root.right.data){
+        // Right Right
+        if (balance < -1 && data > root.right.data) {
             return leftRotate(root);
-            
-        } 
 
-        // Right Left 
-        if (balance < -1 && data < root.right.data){
+        }
+
+        // Right Left
+        if (balance < -1 && data < root.right.data) {
             root.right = this.rightRotate(root.right);
             return this.leftRotate(root);
-        } 
-        
+        }
+
         return root;
 
     }
@@ -97,5 +115,5 @@ public class AVLTree {
 
     public Node getRoot() {
         return root;
-    }  
+    }
 }
--- src/app/avltree/AVLTreeUtilities.java
+++ src/app/avltree/AVLTreeUtilities.java
@@ -2,17 +2,32 @@ package app.avltree;
 
 public class AVLTreeUtilities {
 
+    /**
+     * Compute the height of a node.
+     * 
+     * @param root a {@code Node}
+     * @return height of node. if node is {@code null}, {@code -1} is returned
+     */
     public static int getHeight(Node root) {
         return root == null ? -1 : root.height;
     }
 
+    /**
+     * Update the height of a node returning a new height
+     * 
+     * @param root a {@code Node}
+     * @return the new height of the node
+     */
     public static int getNewHeight(Node root) {
-        return 1 + Math.max(
-            getHeight(root.left), 
-            getHeight(root.right)
-        );
+        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
     }
-    
+
+    /**
+     * Get the balance of a node
+     * 
+     * @param root a {@code Node}
+     * @return balance of node. if node is {@code null}, {@code 0} is returned
+     */
     public static int getBalance(Node root) {
         if (root == null) {
             return 0;
@@ -21,6 +36,4 @@ public class AVLTreeUtilities {
         return getHeight(root.left) - getHeight(root.right);
     }
 
-    
-    
 }
